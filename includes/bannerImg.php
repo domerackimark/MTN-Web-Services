ba32da6a153dccba0fa98dc362fd103770d4a12b
domerackimark@gmail.com <domerackimark@gmail.com>
2021-02-21 09:26:20 -0500

    Adding working files.

---
+++ includes/bannerImg.php
@@ -0,0 +1,26 @@
+<header class="hero-img">   
+      <div class="container">
+        <h1 class="intro-text">MTN Web Services</h1>
+        <span id="quote-text">Quotes will go here...</span> 
+      </div>  
+  </header>
+
+  
+  <style>
+      .hero-img{
+          display: grid;
+          grid-column: 150px;
+          justify-items: center;
+          align-items: center;
+          background-image: url('./assets/matterhorn-507014_1280.jpg');
+          background-position: center;
+          height: 800px;
+          background-size: cover;
+          background-repeat: no-repeat;                 
+      }
+
+      .intro-text{
+          font-size: 40px;
+          padding-left: 150px;
+      }
+  </style>
